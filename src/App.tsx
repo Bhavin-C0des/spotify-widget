e95fd8c88a14cff8b170392a3ec64c7328be4903
Bhavin Melwani <bhavinmelwani@gmail.com>
2026-06-12 18:01:55 +0530

    Added album cover for starboy (hardcoded for now)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Play, Pause, SkipBack, SkipForward } from 'lucide-react'
 import { useState } from 'react'
+import starboyAlbumCover from './assets/starboy.png'
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="bg-gray-900 flex items-center justify-center rounded-4xl gap-4 w-fit px-8 py-2">
-      <div className="size-12 bg-blue-800"></div>
+      <div className="size-12">
+        <img src={starboyAlbumCover} alt="Starboy Album Cover" className="rounded" />
+      </div>
       <div className="flex flex-col text-white w-40">
         <h1 className="text-xl font-bold">Starboy</h1>
         <h2 className="text-lg">Weeknd</h2>
