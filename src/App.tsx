c90cb4d00cd786c5a36b3076b3c96793be25b400
Bhavin Melwani <bhavinmelwani@gmail.com>
2026-06-12 17:48:14 +0530

    Add playback icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { Play, Pause, SkipBack, SkipForward } from 'lucide-react'
+import { useState } from 'react'
+
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="bg-gray-900 flex items-center justify-center rounded-4xl gap-4 w-fit px-8 py-2">
       <div className="size-12 bg-blue-800"></div>
@@ -6,10 +10,11 @@ function App() {
         <h1 className="text-xl font-bold">Starboy</h1>
         <h2 className="text-lg">Weeknd</h2>
       </div>
-      <div className="flex items-center gap-2">
-        <button className="bg-gray-700 text-white px-4 py-2 rounded">Prev</button>
-        <button className="bg-gray-700 text-white px-4 py-2 rounded">Pause</button>
-        <button className="bg-gray-700 text-white px-4 py-2 rounded">Next</button>
+      <div className="flex items-center gap-2 text-white">
+        <SkipBack />
+        <Play onClick={() => setIsPlaying(!isPlaying)} className={isPlaying ? 'hidden' : ''} />
+        <Pause onClick={() => setIsPlaying(!isPlaying)} className={isPlaying ? '' : 'hidden'}/>
+        <SkipForward />
       </div>
     </div>
   )
