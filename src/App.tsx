1ed11640a2fc5aabe46c759a6a512c5b9c759e3d
Bhavin Melwani <bhavinmelwani@gmail.com>
2026-06-12 10:46:55 +0530

    Create initial Spotify widget UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 function App() {
   return (
-    <div className="h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Spotify Widget</h1>
+    <div className="bg-gray-900 flex items-center justify-center rounded-4xl gap-4 w-fit px-8 py-2">
+      <div className="size-12 bg-blue-800"></div>
+      <div className="flex flex-col text-white w-40">
+        <h1 className="text-xl font-bold">Starboy</h1>
+        <h2 className="text-lg">Weeknd</h2>
+      </div>
+      <div className="flex items-center gap-2">
+        <button className="bg-gray-700 text-white px-4 py-2 rounded">Prev</button>
+        <button className="bg-gray-700 text-white px-4 py-2 rounded">Pause</button>
+        <button className="bg-gray-700 text-white px-4 py-2 rounded">Next</button>
+      </div>
     </div>
   )
 }
